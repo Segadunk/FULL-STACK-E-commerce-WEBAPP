41bed18dc8801731b4f823dd905d43c0f8ae7ace
Prz <prajwol.oli.27@gmail.com>
2026-09-04 00:31:31 +0000

    learnt and added email regex

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -9,6 +9,27 @@ const userSchema = new mongoose.Schema({
         type:Number,
         min:[18,"User cannot be under 18"],
     }, 
+    country:{
+        type:String,
+        required:[true,"Country is required"],
+        default:Nepal,
+    },
+    email:{
+        type:String,
+        required:[true,"Email address is required"],
+        minLength:5,
+        maxLength:100,
+        unique: true, //this will make sure that two users do not have the same email
+        lowercase: true,//this will automatically make all the characters into lowercase
+        //now comes a very powerful function
+        validate:{
+            validator: (value)=>{
+                 const emailRegex= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                emailRegex.test(value);
+            },
+            message: "Invalid email address"
+        },
+    },
 })
 
 export default mongoose.model("User",userSchema)
